0cd7974fbd496317825637b9339b2447fe2d947d
Nick <ngordon22293@gmail.com>
2016-02-17 22:03:48 +0000

    Remove 'finish' method fom AppController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  
+ 
   def init
     render text: "Welcome to Seddit" 
   end  
-  
-  def finish  
-    render text: "Thank you for visiting Seddit" 
-  end 
 end
